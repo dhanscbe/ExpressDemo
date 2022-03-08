36a16e451d3b2a8c850b1ac676ade4e0f11fe509
dhanscbe <dhanscbe@gmail.com>
2022-03-08 14:23:35 -0500

    Added validation of the inputs using Joi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//Joi is a class
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 //This line is added since we include req.body.name
@@ -5,9 +7,9 @@ const app = express();
 app.use(express.json());
 
 const courses = [
-	{id: 1,	name: 'Javascript'},
-	{id: 2, name:'Java'},
-	{id: 3, name: 'Spring'}
+	{ id: 1, name: 'Javascript' },
+	{ id: 2, name: 'Java' },
+	{ id: 3, name: 'Spring' }
 ];
 
 app.get('/', (req, res) => {
@@ -20,20 +22,38 @@ app.get('/courses', (req, res) => {
 
 app.get('/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if(!course)
-	res.status(404).send('Course id does not exist');
+	if (!course)
+		res.status(404).send('Course id does not exist');
 	else
-	res.send(course.name);
+		res.send(course.name);
 });
 
-app.post('/courses',(req,res) => {
-	const course = {
-		id : courses.length + 1,
-		name : req.body.name
-	};
-	courses.push(course);
-	res.send(course);
-})
+app.post('/courses', (req, res) => {
+
+	/*	if(req.body.name == null || req.body.name.length < 6){
+		res.status(400).send('Name is required or name should be minimum 6 characters');
+		return;
+	}*/
+
+	const schema = Joi.object({
+		name: Joi.string().min(6).required()
+	});
+	const result = schema.validate(req.body);
+	console.log(result);
+
+	if (result.error) {
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
+	else {
+		const course = {
+			id: courses.length + 1,
+			name: req.body.name
+		};
+		courses.push(course);
+		res.send(courses);
+	}
+});
 
 const port = process.env.PORT || 8082;
 app.listen(port, () =>
